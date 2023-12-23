950361518cbe82e0ee100d43eff87e83d9ef89d4
Jan T. Sott <git@idleberg.com>
2023-12-24 00:41:58 +0100

    move build config

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	clean: true,
+	dts: true,
+	entry: ['index.ts'],
+	format: 'esm',
+	minify: true,
+	platform: 'node',
+	treeshake: 'recommended'
+});
